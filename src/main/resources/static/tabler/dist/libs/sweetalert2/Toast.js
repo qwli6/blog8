fb053795b9221d401b134355b4cdad0a85618f8f
selfassu <selfassu@gmail.com>
2020-07-15 23:38:28 +0800

    add sweet2alert.js

--- src/main/resources/static/tabler/dist/libs/sweetalert2/Toast.js
+++ src/main/resources/static/tabler/dist/libs/sweetalert2/Toast.js
@@ -2,7 +2,7 @@ const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    timer: 3000,
+    timer: 800,
     timerProgressBar: true,
     onOpen: (toast) => {
         toast.addEventListener('mouseenter', Swal.stopTimer)
