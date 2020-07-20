ab6a41751d7e3aaf7ac059391f6eb84ca55e0f59
selfassu <selfassu@gmail.com>
2020-07-20 14:01:36 +0800

    enhance -> 🔧 LoginParam add attr -> rememberMe

--- src/main/java/me/lqw/blog8/model/vo/LoginParam.java
+++ src/main/java/me/lqw/blog8/model/vo/LoginParam.java
@@ -5,6 +5,11 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
+/**
+ * 登录入参
+ * @author liqiwen
+ * @version 1.0
+ */
 public class LoginParam implements Serializable {
 
     @NotBlank(message = "please provide an username.")
@@ -14,6 +19,20 @@ public class LoginParam implements Serializable {
     @NotBlank(message = "please provide a password.")
     @Length(min = 4, max = 64, message = "username length must between [4, 64].")
     private String password;
+
+    /**
+     * 记住我, 默认不记住
+     * @since 1.2
+     */
+    private Boolean rememberMe = false;
+
+    public Boolean getRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(Boolean rememberMe) {
+        this.rememberMe = rememberMe;
+    }
 
     public String getUsername() {
         return username;
