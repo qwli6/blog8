c002492809cd764d4343a1eaf25c1d5164aebe52
selfassu <selfassu@gmail.com>
2020-07-20 14:02:25 +0800

    enhance -> add UnauthorizedException

--- src/main/java/me/lqw/blog8/exception/UnauthorizedException.java
+++ src/main/java/me/lqw/blog8/exception/UnauthorizedException.java
@@ -1,6 +1,11 @@
 package me.lqw.blog8.exception;
 
-
+/**
+ * 未认证异常
+ * @author liqiwen
+ * @version 1.0
+ * @since 1.0
+ */
 public class UnauthorizedException extends LogicException {
 
     public UnauthorizedException(String code, String msg) {
