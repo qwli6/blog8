c2516f1af9a817cade2f8bc7e90f57a5cdad26b5
selfassu <selfassu@gmail.com>
2020-07-25 22:53:16 +0800

    媒体处理工具

--- src/main/java/me/lqw/blog8/file/MediaTool.java
+++ src/main/java/me/lqw/blog8/file/MediaTool.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 /**
  * 媒体工具处理类
  * 处理视频，流媒体, 图片
+ * 利用 ffpemg 来处理音视频
  * @author liqiwen
  * @version 1.2
  * @since 1.2
@@ -36,35 +37,80 @@ public class MediaTool {
         super();
     }
 
-
+    /**
+     * 判断文件是否是以 jpeg 结尾的文件
+     * @param ext ext
+     * @return boolean
+     */
     public static boolean isJPEG(String ext){
         return JPEG.equalsIgnoreCase(ext) || JPG.equalsIgnoreCase(ext);
     }
 
+    /**
+     * 判断文件是否是 png 结尾的文件
+     * @param ext ext
+     * @return Boolean
+     */
     public static boolean isPNG(String ext){
         return PNG.equalsIgnoreCase(ext);
     }
 
+    /**
+     * 判断文件是否是 gif 结尾的文件
+     * @param ext ext
+     * @return boolean
+     */
     public static boolean isGIF(String ext){
         return GIF.equalsIgnoreCase(ext);
     }
 
+    /**
+     * 判断文件是否是 webp 结尾的文件
+     * @param ext ext
+     * @return boolean
+     */
     public static boolean isWEBP(String ext) {
         return WEBP.equalsIgnoreCase(ext);
     }
 
+    /**
+     * 判断文件是否是可被处理的图片
+     * @param ext ext
+     * @return boolean
+     */
     public static boolean isProcessableImage(String ext){
         return isGIF(ext) || isPNG(ext) || isJPEG(ext) || isWEBP(ext);
     }
 
+    /**
+     * 判断文件是否是可被处理的视频
+     * @param ext ext
+     * @return boolean
+     */
     public static boolean isProcessableVideo(String ext){
         return MP4.equalsIgnoreCase(ext) || MOV.equalsIgnoreCase(ext);
     }
 
+    /**
+     * 从文件中读取文件信息
+     * @param src src
+     * @return ImageInfo
+     */
     public ImageInfo readImageInfo(Path src) {
 
 
         return new ImageInfo();
+    }
+
+    /**
+     * 从文件中读取视频信息
+     * @param src src
+     * @return VideoInfo
+     */
+    public VideoInfo readVideoInfo(Path src){
+
+
+        return new VideoInfo(0, 0, 20);
     }
 
     public static File toWebpFile(String imageFilePath){
@@ -83,7 +129,14 @@ public class MediaTool {
     }
 
 
-
+    /**
+     * 图片缩放处理
+     * @param srcImg src
+     * @param destImg destImage
+     * @param width width
+     * @param height height
+     * @param equalScale equalScale
+     */
     public static void resize(File srcImg, File destImg, int width, int height, boolean equalScale){
         Optional<String> extOp = FileUtils.getExtension(srcImg);
         if(extOp.isPresent()) {
