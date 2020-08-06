feca695050eea992f90b7b927fc586e2bf94ef1a
selfassu <selfassu@gmail.com>
2020-08-06 22:19:50 +0800

    优化代码

--- src/main/java/me/lqw/blog8/model/vo/HandledArticlePageQueryParam.java
+++ src/main/java/me/lqw/blog8/model/vo/HandledArticlePageQueryParam.java
@@ -1,5 +1,7 @@
 package me.lqw.blog8.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import me.lqw.blog8.model.Category;
 import me.lqw.blog8.model.Tag;
 import me.lqw.blog8.validator.StatusEnum;
@@ -7,6 +9,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 文章内容查询参数
@@ -15,12 +18,60 @@ import java.time.LocalDateTime;
  */
 public class HandledArticlePageQueryParam extends PageQueryParam implements Serializable {
 
-
+    /**
+     * 是否查询私人内容
+     */
     private boolean queryPrivate;
 
+    /**
+     * 是否查询受密码保护的内容
+     */
     private boolean queryPasswordProtect;
 
+    /**
+     * 分页查询参数
+     */
     private PageQueryParam pageQueryParam;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime begin;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime end;
+
+    /**
+     * 文章 id，从 lucene 根据 query/category/tag 获取文章列表，
+     * 查询出文章的 id，然后进行检索
+     *
+     * 返给前端的时候，忽略这个字段
+     */
+    @JsonIgnoreProperties
+    private List<Integer> ids;
+
+    /**
+     * 分类
+     */
+    private Category category;
+
+    /**
+     * 标签
+     */
+    private Tag tag;
+
+    /**
+     * 查询关键字段
+     * 需要对长度进行限制
+     */
+    private String query;
+
+    /**
+     * 查询内容的状态
+     * 用户未登录|POSTED
+     * 用户已登录|ALL
+     */
+    private StatusEnum status;
 
     public PageQueryParam getPageQueryParam() {
         return pageQueryParam;
@@ -46,14 +97,6 @@ public class HandledArticlePageQueryParam extends PageQueryParam implements Seri
         this.queryPasswordProtect = queryPasswordProtect;
     }
 
-    private Category category;
-
-    private Tag tag;
-
-    private String query;
-
-    private StatusEnum status;
-
     public String getQuery() {
         return query;
     }
@@ -70,7 +113,6 @@ public class HandledArticlePageQueryParam extends PageQueryParam implements Seri
         this.status = status;
     }
 
-
     public Tag getTag() {
         return tag;
     }
@@ -78,12 +120,6 @@ public class HandledArticlePageQueryParam extends PageQueryParam implements Seri
     public void setTag(Tag tag) {
         this.tag = tag;
     }
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime begin;
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime end;
 
     public LocalDateTime getBegin() {
         return begin;
@@ -109,14 +145,21 @@ public class HandledArticlePageQueryParam extends PageQueryParam implements Seri
         this.category = category;
     }
 
+    public List<Integer> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Integer> ids) {
+        this.ids = ids;
+    }
+
     public HandledArticlePageQueryParam(ArticlePageQueryParam queryParam, Integer categoryId, Integer tagId) {
+        super();
         this.pageQueryParam = queryParam;
         if(categoryId != null) {
             this.category = new Category();
             category.setId(categoryId);
         }
-//        this.set = queryParam.getCurrentPage();
-//        this.pageSize = queryParam.getPageSize();
         this.query = queryParam.getQuery();
         this.status = queryParam.getStatus();
         if(tag != null){
@@ -125,5 +168,7 @@ public class HandledArticlePageQueryParam extends PageQueryParam implements Seri
     }
 
     public HandledArticlePageQueryParam() {
+        super();
     }
+
 }
--- src/main/java/me/lqw/blog8/web/filter/ContextFilter.java
+++ src/main/java/me/lqw/blog8/web/filter/ContextFilter.java
@@ -2,6 +2,7 @@ package me.lqw.blog8.web.filter;
 
 import me.lqw.blog8.BlogConstants;
 import me.lqw.blog8.BlogContext;
+import me.lqw.blog8.model.User;
 import me.lqw.blog8.model.config.BlogConfigModel;
 import me.lqw.blog8.service.BlogConfigService;
 import me.lqw.blog8.util.WebUtil;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -36,6 +38,15 @@ public class ContextFilter implements Filter {
         BlogContext.setIp(ip);
 
         logger.info("ContextFilter#doFilter...");
+
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        if(session == null){
+            session = ((HttpServletRequest) request).getSession(true);
+        }
+        User user = (User) session.getAttribute(BlogConstants.AUTH_USER);
+        BlogContext.AUTH_THREAD_LOCAL.set(user != null);
+
+        logger.info("用户是否登录：[{}]", BlogContext.isAuthorized());
 
 
         logger.debug("Start load blog config info");
