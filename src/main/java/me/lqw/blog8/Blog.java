6902a5ec8dc04c49c5447b4fd2f2e252d10b273b
selfassu <selfassu@gmail.com>
2020-08-14 23:37:26 +0800

    rename

--- src/main/java/me/lqw/blog8/Blog.java
+++ src/main/java/me/lqw/blog8/Blog.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * Blog 启动类
+ * @author liqiwen
+ * @since 1.2
+ * @version 1.2
+ */
 @EnableCaching
 @SpringBootApplication
-public class Blog8Application {
+public class Blog {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Blog8Application.class, args);
+		SpringApplication.run(Blog.class, args);
 	}
 
 }
